4e1fa9390a11d613009ff44be23c323ddafd7c21
Eric Hernandez <herneri@protonmail.com>
2024-02-24 18:45:00 -0600

    refactor: Seperate searching into two functions and get rid of unneccesary code

--- database.cpp
+++ database.cpp
@@ -31,8 +31,7 @@ class Database {
 
 	public:
 		enum operation_values {
-			PRINT_RESULTS = 1, GET_INSTANCES, DB_REMOVE,
-			DB_REPLACE, DB_INSERT
+			DB_REMOVE, DB_REPLACE, DB_INSERT
 		};
 
 		/* The database connection opened here must be closed
@@ -77,20 +76,39 @@ class Database {
 			return;
 		}
 
-		static int search(int qtype) {
+		static void general_search() {
+			sqlite3_stmt *prepared_statement;
+
+			enum columns {
+				PATH, NAME
+			};
+
+			sql_statement = "SELECT path, name FROM package WHERE name = '" + (string) target + "';";
+			status = sqlite3_prepare(db, sql_statement.c_str(), -1, &prepared_statement, NULL);
+			if (status < 0) {
+				cout << error_message << endl;
+				return;
+			}
+
+			while (status = sqlite3_step(prepared_statement) == SQLITE_ROW) {
+				cout << (char *) sqlite3_column_text(prepared_statement, PATH) << (char *) sqlite3_column_text(prepared_statement, NAME) << endl;
+			}
+
+			sqlite3_finalize(prepared_statement);
+			return;
+		}
+
+		static int instance_search() {
 			struct id_list::id_node *ids = NULL;
 			sqlite3_stmt *prepared_statement;
 			string package_name;
-			char *column_name;
-			bool is_name = false;
 			int selected_id;
-			query_type = qtype;
 
 			enum column {
-				PATH, NAME, ID
+				PATH, ID
 			};
 
-			sql_statement = "SELECT path, name, id FROM package WHERE name = '" + (string) target + "';";
+			sql_statement = "SELECT path, id FROM package WHERE name = '" + (string) target + "';";
 			status = sqlite3_prepare(db, sql_statement.c_str(), -1, &prepared_statement, NULL);
 			if (status < 0) {
 				cout << error_message << endl;
@@ -98,41 +116,23 @@ class Database {
 			}
 
 			while (status = sqlite3_step(prepared_statement) == SQLITE_ROW) {
-				for (int i = 0; i <= 2; i++) {
-					column_name = (char *) sqlite3_column_text(prepared_statement, i);
-
-					if (query_type == GET_INSTANCES && is_name == true && i == ID) {
-						ids = id_list::add_tail(ids, sqlite3_column_int(prepared_statement, i), (char *) package_name.c_str());
-						is_name = false;
-						continue;
-					}
-
-					if (i == PATH) {
-						package_name = column_name;
-						continue;
-					}
-
-					if (i == NAME) {
-						package_name = package_name + column_name;
-						if (query_type == PRINT_RESULTS) {
-							cout << package_name << endl;
-							continue;
-						}
-
-						is_name = true;
-					}
-				}
+				package_name = (char *) sqlite3_column_text(prepared_statement, PATH);
+				package_name = package_name + target;
+				ids = id_list::add_tail(ids, sqlite3_column_int(prepared_statement, ID), (char *) package_name.c_str());
 			}
 
 			sqlite3_finalize(prepared_statement);
-
 			id_list::print_list(ids);
 			cout << "Multiple packages named '" + (string) target + "' found" << endl;
 			cout << "Select id of one: ";
 			cin >> selected_id;
 
-			id_list::search_for_id(ids, selected_id);
-			return 0;
+			status = id_list::search_for_id(ids, selected_id);
+			if (status != 0) {
+				return -1;
+			}
+
+			return selected_id;
 		}
 
 		/* Provides insertion and deletion in the database */
