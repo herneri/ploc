981d6137514721492f89fe30bc3d68945d69147b
Eric Hernandez <herneri@protonmail.com>
2025-02-26 12:36:40 -0600

    feat: Process CLI options

--- main.c
+++ main.c
@@ -39,6 +39,79 @@ int main(int argc, char *argv[]) {
 		return PLOC_INIT_FAIL;
 	}
 
+	for (int i = 1; i < argc; i++) {
+		if (arg_mode == FIND_ARG && strnlen(argv[i], 255) < 2) {
+			fprintf(stderr, "ploc: An argument must be at least 2 characters long\n");
+			return PLOC_ARG_SYNTAX_ERR;
+		}
+
+		if (arg_mode == FIND_ARG && argv[i][0] == '-') {
+			arg_mode = GET_OPERATION;
+		} else if (arg_mode == FIND_ARG) {
+			fprintf(stderr, "ploc: Option must come before operand\n");
+			return PLOC_ARG_SYNTAX_ERR;
+		}
+
+		if (arg_mode == GET_OPERATION && i + 1 >= argc) {
+			fprintf(stderr, "ploc: Not enough arguments\n");
+			return PLOC_ARG_SYNTAX_ERR;
+		}
+
+		switch (argv[i][1]) {
+		case 'i':
+			if (i + 2 >= argc) {
+				fprintf(stderr, "ploc: Not enough arguments\n");
+				return PLOC_ARG_SYNTAX_ERR;
+			}
+
+			if (strchr(argv[i + 2], '/') == NULL) {
+				fprintf(stderr, "ploc: Destination must be an absolute path\n");
+				return PLOC_ARG_SYNTAX_ERR;
+			}
+
+			strcpy(pkg.path, argv[i + 2]);
+			ploc_install_package(database_connection, &pkg, argv[i + 1]);
+
+			i += 2;
+			break;
+		case 'r':
+			strcpy(pkg.name, argv[i + 1]);
+			ploc_check_conflict(database_connection, false, &pkg);
+			ploc_delete_package(database_connection, &pkg);
+
+			i++;
+			break;
+		case 'l':
+			strcpy(pkg.name, argv[i + 1]);
+			ploc_check_conflict(database_connection, false, &pkg);
+			if (strncmp(pkg.path, "NULL", 255) == 0) {
+				fprintf(stderr, "ploc: %s not found\n", pkg.name);
+				return PLOC_CONFLICT;
+			}
+
+			ploc_database_load_info(database_connection, &pkg);
+			ploc_list_package(&pkg);
+			i++;
+			arg_mode = FIND_ARG;
+			break;
+		case 's':
+			ploc_database_fetch_all(database_connection, argv[i + 1]);
+			i++;
+			break;
+		case 'S':
+			ploc_get_name_and_path(&pkg, argv[i + 1]);
+			ploc_database_search_unique(database_connection, &pkg);
+
+			i++;
+			arg_mode = FIND_ARG;
+			break;
+		default:
+			fprintf(stderr, "ploc: Invalid argument: %s\n", argv[i]);
+			return PLOC_ARG_SYNTAX_ERR;
+		}
+
+	}
+
 	sqlite3_close(database_connection);
 	return PLOC_OK;
 }
